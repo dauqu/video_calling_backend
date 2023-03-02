3e653f8e17a9597072cd586407aed1d2236b7db8
Harsh Singh <sujeetsingh734321@gmail.com>
2023-03-02 18:16:00 +0530

    cors

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { Server } = require("socket.io");
+
+
+// allow cross origin requests
+const allowedOrigins = ["http://localhost:3000", "http://localhost:8000", "http://localhost:8001", "https://video-calling-nine.vercel.app"];
+
 const io = new Server({
-  cors: true,
+  // allow cross origin requests
+  cors: {
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+
 });
+
+const corss = require('cors');
+app.use(corss({
+  origin: allowedOrigins,
+  credentials: true
+
+}))
+
 const app = express();
 const HTTTP_PORT = 8000;
 const SOCKET_PORT = 8001;
@@ -21,7 +40,7 @@ const socketToEmailMapping = new Map();
 
 io.on("connection", (socket) => {
   console.log("New Connection");
-  
+
   socket.on("join-room", (data) => {
     const { roomId, emailId } = data;
     console.log(`User ${emailId} joined room ${roomId}`);
